3a7b3e40f4e30f5eff2670315696dde8fe8cacdd
MyFrondEndProjects <usscrean1@outlook.com>
2024-03-17 17:46:06 +0300

    Api eklendi
    Html, css ve Javascript kodları yazıldı ve Api eklendi

--- Js/App.js
+++ Js/App.js
@@ -1,1 +1,18 @@
-alert("");
+const imgsearch=document.querySelector(".imgsearch");
+const searchbutton=document.querySelector(".searchbutton");
+const Temizlebutton=document.querySelector(".Temizle");
+const searchcontainer=document.querySelector(".search-container");
+const imagecontainer=document.querySelector(".image-container");
+const container=document.querySelector(".container");
+const Form=document.querySelector(".Form");
+
+
+function search (e)
+{
+const value=imgsearch.value.Trim(); // Trim(); yazarak İmgsearch class lı inputun değerini boşluk gibi değerlerden temizleyerek ata dedim
+fetch(`https://api.unsplash.com/search/photos?query=$(value)`);
+
+    e.preventDefault();
+}
+
+Form.addEventListener("submit",search);
